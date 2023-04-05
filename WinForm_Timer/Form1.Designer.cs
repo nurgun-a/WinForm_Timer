2ab27eda63d011e89bdd9f57ed430776d43d7250
nurgun_a <nurgun.alekseev@bk.ru>
2023-04-05 12:47:22 +0300

    Добавьте файлы проекта.

---
+++ WinForm_Timer/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace WinForm_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.t1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.t1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start_button.Location = new System.Drawing.Point(12, 80);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(70, 65);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "СТАРТ";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // stop_button
+            // 
+            this.stop_button.Location = new System.Drawing.Point(101, 80);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(65, 65);
+            this.stop_button.TabIndex = 0;
+            this.stop_button.Text = "СТОП";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // t1
+            // 
+            this.t1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.t1.Location = new System.Drawing.Point(12, 26);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(154, 36);
+            this.t1.TabIndex = 1;
+            this.t1.Click += new System.EventHandler(this.t1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(185, 157);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.start_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Таймер";
+            ((System.ComponentModel.ISupportInitialize)(this.t1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.NumericUpDown t1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WinForm_Timer/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Timer
+{    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            stop_button.Enabled = false;
+            start_button.Enabled = false;
+            timer1.Tick += new EventHandler(Show_timer1);
+        }
+        private void Show_timer1(object sender, EventArgs e)
+        {
+            if (t1.Value > 0) t1.Value--;
+            else if (t1.Value < 1) 
+            {
+                timer1.Stop();
+                MessageBox.Show("Таймер остановлен", "Таймер", MessageBoxButtons.OK);
+            }
+            
+        }
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            if (t1.Value>0)
+            {
+                stop_button.Enabled = true;
+                start_button.Enabled = false;
+                timer1.Start();
+            }
+            
+        }
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            DialogResult res = MessageBox.Show($"Остаток времени: {t1.Value} секунд\nВозобновить?", "Таймер", MessageBoxButtons.YesNo);
+            if (res == DialogResult.Yes)
+            {
+                stop_button.Enabled = false;
+                start_button.Enabled = true;
+            }
+            else if (res == DialogResult.No)
+            {
+                t1.Value = 0;
+                stop_button.Enabled = false;
+            }
+           
+        }
+        private void t1_Click(object sender, EventArgs e)
+        {
+            if (t1.Value>0)
+            {
+                start_button.Enabled = true;
+            }
+        }
+    }
+}
